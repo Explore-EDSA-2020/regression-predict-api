e631f5323d64acb2b25db0d1c95b7877ca39d261
AllieUbisse <AllieSilverUbisse@gmail.com>
2020-06-02 09:29:36 +0000

    added feature engineering functions

--- model.py
+++ model.py
@@ -45,6 +45,134 @@ def _preprocess_data(data):
         The preprocessed data, ready to be used our model for prediction.
 
     """
+    ##########################################################################
+    #                START OF TEAM 10 FEATURE ENGINEERING FUNCTIONS
+    ##########################################################################
+
+    ################################### drop_columns  ########################
+    # To drop columns
+    features_to_drop = ['User Id',
+                    'Rider Id',
+                    'Order No', # not for test
+                    'Vehicle Type',
+                    'Precipitation in millimeters',
+                    'Arrival at Destination - Day of Month',
+                    'Arrival at Destination - Weekday (Mo = 1)',
+                    'Confirmation - Day of Month',
+                    'Confirmation - Weekday (Mo = 1)',
+                    'Arrival at Pickup - Day of Month',
+                    'Arrival at Pickup - Weekday (Mo = 1)',
+                    'Pickup - Day of Month', 'Pickup - Weekday (Mo = 1)',
+                    'Pickup - Weekday (Mo = 1)'
+                    'Arrival at Destination - Day of Month',
+                    ]
+
+    def drop_columns(df, features_to_drop = features_to_drop):
+        
+        return df.drop([column for column in df.columns if column in features_to_drop], axis=1)
+    
+
+    ################################## to_platform_type  ########################################
+    def to_platform_type(df):
+        """Returns a dataframe with encoded platform feature"""
+
+        types = {1:'Type 1',2:'Type 2',3:'Type 3',4:'Type 4'}
+
+        df['Platform Type'] = df['Platform Type'].map(types)
+        
+        return df
+
+    ############################## to_weekday_name ########################################
+    def to_weekday_name(df):
+        """Returns a dataframe with names of days of the week"""
+
+
+        ends_with = 'Weekday (Mo = 1)'
+        weekday_names = {1:"Monday", 2:"Tuesday", 3: "Wednesday", 4:"Thursday", 5:"Friday", 6: "Saturday", 7:"Sunday"}
+        for col in df.columns:
+            if col.endswith(ends_with):
+                df[col] = df[col].map(weekday_names) 
+        
+        return df
+
+    ###############################Time Based Features###################################
+    # Time Based Features
+    #--------------------
+    # 'Placement - Time'
+    # 'Confirmation - Time' 
+    # 'Arrival at Pickup - Time'
+    # 'Pickup - Time'
+    # 'Arrival at Destination - Time'
+    # ------------------
+
+
+    time_features = ['Placement - Time',
+                    'Confirmation - Time' ,
+                    'Arrival at Pickup - Time',
+                    'Pickup - Time',
+                    'Arrival at Destination - Time',
+                    ]
+
+
+    def to_seconds(df, time_features=time_features):
+        """Returns a Dataframe with time features converted to seconds"""
+
+        for column in time_features:
+
+            if column in df:
+            df[column] = pd.to_datetime(df[column])
+
+            df[column.split(' -')[0]+'_hour'] = df[column].dt.hour
+            df[column.split(' -')[0]+'_minute'] = df[column].dt.minute
+            df[column.split(' -')[0]+'_second'] = df[column].dt.second
+            df[column]= (df[column] - pd.to_datetime(pd.to_datetime('today').date())).astype('timedelta64[s]')
+        
+        return df
+
+
+    ##########################################################################
+    # Delta Features
+    # ----------------------
+
+    # -----------------------
+    # Train and Test
+    # ----------------
+    # 'delta-Time-Confirmation_Placement'           ---> 'Confirmation - Time', 'Placement - Time'
+    # 'delta-Time-Arrival-at-Pickup_Confirmation'   ---> 'Arrival at Pickup - Time', 'Confirmation - Time'
+    # 'delta-Time-Pickup_Arrival-at-Pickup'         ---> 'Pickup - Time', 'Arrival at Pickup - Time'
+    # ----------------
+    # Train Not in TEST
+    # ----------------
+    # 'delta-Time-Arrival-at-Destination_Pickup'    ---> 'Arrival at Destination - Time', 'Pickup - Time'
+    # 'delta-Time-Arrival-at-Destination_Placement' ---> 'Arrival at Destination - Time', 'Placement - Time'
+
+    delta_cols = [
+                ('delta-Time-Confirmation_Placement',  'Confirmation - Time', 'Placement - Time' ),
+                ('delta-Time-Arrival-at-Pickup_Confirmation' ,'Arrival at Pickup - Time', 'Confirmation - Time'),
+                ('delta-Time-Pickup_Arrival-at-Pickup', 'Pickup - Time', 'Arrival at Pickup - Time'),
+                ('delta-Time-Arrival-at-Destination_Pickup', 'Arrival at Destination - Time', 'Pickup - Time'),
+                ('delta-Time-Arrival-at-Destination_Placement','Arrival at Destination - Time', 'Placement - Time')
+                ]
+
+    # Function to compute to delta
+    def to_delta(df, delta_cols=delta_cols):
+        """Returns delta features from existing features"""
+
+        for deltas in delta_cols:                               # Loop over the List of tuple [(output_name, col1, col2 ), (output_name, col1, col2 )]
+
+            if deltas[1] in df and deltas[2] in df:             # check if columns exists
+                df[deltas[0]] = df[deltas[1]] - df[deltas[2]]   # make delta feaure
+
+        return df
+
+
+
+    ##########################################################################
+    #                END OF TEAM 10 FEATURE ENGINEERING FUNCTIONS
+    ##########################################################################
+
+
+
     # Convert the json string to a python dictionary object
     feature_vector_dict = json.loads(data)
     # Load the dictionary as a Pandas DataFrame.
